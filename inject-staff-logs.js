78ff959fe5f143ef6fdbca52655ac357f4602850
cvnt <cvntypussy@outlook.com>
2026-09-12 21:13:18 +0200

    Fix channel.send this-binding crash

--- inject-staff-logs.js
+++ inject-staff-logs.js
@@ -1,10 +1,9 @@
 const fs = require("fs");
-const path = require("path");
 
 function walk(dir, out = []) {
   if (!fs.existsSync(dir)) return out;
   for (const e of fs.readdirSync(dir, { withFileTypes: true })) {
-    const p = path.join(dir, e.name);
+    const p = require("path").join(dir, e.name);
     if (e.isDirectory()) walk(p, out);
     else if (e.name.endsWith(".js")) out.push(p);
   }
@@ -13,28 +12,23 @@ function walk(dir, out = []) {
 
 const files = ["/zeppelin/backend/dist", "/zeppelin/backend/src"].flatMap((r) => walk(r));
 
-function writeIfChanged(file, next, orig) {
-  if (next !== orig) {
-    fs.writeFileSync(file, next);
-    console.log("[PATCH]", file);
-  }
-}
-
 for (const file of files) {
   let s = fs.readFileSync(file, "utf8");
   const orig = s;
 
-  s = s.replace(/void\s+msg\.channel\.send\(/g, "void (msg.channel && msg.channel.send)(");
-  s = s.replace(/void\s+message\.channel\.send\(/g, "void (message.channel && message.channel.send)(");
+  s = s.replace(/void\s+\(msg\.channel && msg\.channel\.send\)\(/g, "void msg.channel?.send(");
+  s = s.replace(/void\s+\(message\.channel && message\.channel\.send\)\(/g, "void message.channel?.send(");
+  s = s.replace(/void\s+msg\.channel\.send\(/g, "void msg.channel?.send(");
+  s = s.replace(/void\s+message\.channel\.send\(/g, "void message.channel?.send(");
 
   if (file.endsWith("ApiPermissionAssignments.js") && s.includes("addUser")) {
-    s = s.replace(
-      /this\.apiPermissions\.insert\(/g,
-      "this.apiPermissions.save("
-    );
+    s = s.replace(/this\.apiPermissions\.insert\(/g, "this.apiPermissions.save(");
   }
 
-  writeIfChanged(file, s, orig);
+  if (s !== orig) {
+    fs.writeFileSync(file, s);
+    console.log("[PATCH]", file);
+  }
 }
 
-console.log("[PATCH] done, files=", files.length);
+console.log("[PATCH] done");
